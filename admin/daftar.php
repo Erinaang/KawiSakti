333934659b309963f8cec2b0fbab8385eb6f598d
KikiAbdullah <kikirabdullah@gmail.com>
2020-05-02 20:00:36 +0700

    direct to logout

--- admin/daftar.php
+++ admin/daftar.php
@@ -22,7 +22,16 @@ if (isset($_POST['daftarSubmit'])) {
 	$password = randomPass();
 	$passEnc = md5($password);
 
-	if ($nama) {
+	$sql_u = "SELECT * FROM user WHERE username='$username'";
+	$sql_e = "SELECT * FROM user WHERE email='$email'";
+	$res_u = mysqli_query($mysqli, $sql_u) or die("data salah: " . mysqli_error($mysqli));
+	$res_e = mysqli_query($mysqli, $sql_e) or die("data salah: " . mysqli_error($mysqli));
+
+	if (mysqli_num_rows($res_u) > 0) {
+		$name_error = "Sorry... username already taken";
+	} else if (mysqli_num_rows($res_e) > 0) {
+		$email_error = "Sorry... email already taken";
+	} else {
 		error_reporting(E_ALL);
 		require '../PHPMailer/src/PHPMailer.php';
 		require '../PHPMailer/src/SMTP.php';
@@ -54,8 +63,6 @@ if (isset($_POST['daftarSubmit'])) {
 		} else {
 			echo error_reporting(E_ALL);
 		}
-	} else {
-		echo "password beda";
 	}
 }
 ?>
@@ -75,10 +82,20 @@ if (isset($_POST['daftarSubmit'])) {
 		<div class="regisForm">
 			<form action="" method="post">
 				<input type="text" name="nama" placeholder="Nama" required="">
-				<input type="email" name="email" placeholder="Email" required="">
+				<div <?php if (isset($email_error)) : ?> class="form_error" <?php endif ?>>
+					<input type="email" name="email" placeholder="Email" required="">
+					<?php if (isset($email_error)) : ?>
+						<span><?php echo $email_error; ?></span>
+					<?php endif ?>
+				</div>
 				<input type="text" name="alamat" placeholder="Alamat" required="">
 				<input type="text" name="telp" placeholder="Nomor Telp" required="">
-				<input type="text" name="username" placeholder="Username" required="">
+				<div <?php if (isset($name_error)) : ?> class="form_error" <?php endif ?>>
+					<input type="text" name="username" placeholder="Username" required="">
+					<?php if (isset($name_error)) : ?>
+						<span><?php echo $name_error; ?></span>
+					<?php endif ?>
+				</div>
 				<!-- <input type="password" name="password" placeholder="Password" required="">
 				<input type="password" name="confirm_password" placeholder="Konfirmasi Password" required=""> -->
 				<div class="tbl-kirim">
