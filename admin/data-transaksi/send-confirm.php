e3ee7ad5d41a7cb2dcfadd3b0293559ddd4ba38d
KikiAbdullah <kikirabdullah@gmail.com>
2020-05-03 12:01:46 +0700

    add alert confirm

--- admin/data-transaksi/send-confirm.php
+++ admin/data-transaksi/send-confirm.php
@@ -5,6 +5,7 @@ $id_transaksi = $GET['id_transaksi'];
 $queryConfirm = mysqli_query($mysqli, "UPDATE transaksi SET status='Diproses' WHERE id_transaksi='$id_transaksi'") or die("data salah: " . mysqli_error($mysqli));
 
 if ($queryConfirm) {
-    header("Location: data-transaksi.php");
+    // header("Location: data-transaksi.php");
+    echo "<script>alert('Transaksi telah diproses.');location.href='data-transaksi.php'</script>";
 }
 ?>
