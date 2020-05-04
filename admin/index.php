55de5ad5f51e1aded29318e66b17bdf94300afb9
KikiAbdullah <kikirabdullah@gmail.com>
2020-05-04 22:41:50 +0700

    sidebar index admin

--- admin/index.php
+++ admin/index.php
@@ -21,53 +21,53 @@ if (!isset($_SESSION["username"])) {
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
     <!-- Bootstrap CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- Bootstrap CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
     <!-- nalika Icon CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/nalika-icon.css">
+    <link rel="stylesheet" href="css/nalika-icon.css">
     <!-- owl.carousel CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/owl.carousel.css">
-    <link rel="stylesheet" href="../css/owl.theme.css">
-    <link rel="stylesheet" href="../css/owl.transitions.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.css">
+    <link rel="stylesheet" href="css/owl.transitions.css">
     <!-- animate CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="css/animate.css">
     <!-- normalize CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="css/normalize.css">
     <!-- meanmenu icon CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/meanmenu.min.css">
+    <link rel="stylesheet" href="css/meanmenu.min.css">
     <!-- main CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="css/main.css">
     <!-- morrisjs CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/morrisjs/morris.css">
+    <link rel="stylesheet" href="css/morrisjs/morris.css">
     <!-- mCustomScrollbar CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/scrollbar/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" href="css/scrollbar/jquery.mCustomScrollbar.min.css">
     <!-- metisMenu CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/metisMenu/metisMenu.min.css">
-    <link rel="stylesheet" href="../css/metisMenu/metisMenu-vertical.css">
+    <link rel="stylesheet" href="css/metisMenu/metisMenu.min.css">
+    <link rel="stylesheet" href="css/metisMenu/metisMenu-vertical.css">
     <!-- calendar CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/calendar/fullcalendar.min.css">
-    <link rel="stylesheet" href="../css/calendar/fullcalendar.print.min.css">
+    <link rel="stylesheet" href="css/calendar/fullcalendar.min.css">
+    <link rel="stylesheet" href="css/calendar/fullcalendar.print.min.css">
     <!-- style CSS
         ============================================ -->
     <link rel="stylesheet" href="style.css">
     <!-- responsive CSS
         ============================================ -->
-    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" href="css/responsive.css">
     <!-- modernizr JS
         ============================================ -->
-    <script src="../js/vendor/modernizr-2.8.3.min.js"></script>
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 
 
 
@@ -225,57 +225,57 @@ if (!isset($_SESSION["username"])) {
             </div>
         </div>
 
-        <script src="../js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="js/vendor/jquery-1.12.4.min.js"></script>
         <!-- bootstrap JS
         ============================================ -->
-        <script src="../js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
         <!-- wow JS
         ============================================ -->
-        <script src="../js/wow.min.js"></script>
+        <script src="js/wow.min.js"></script>
         <!-- price-slider JS
         ============================================ -->
-        <script src="../js/jquery-price-slider.js"></script>
+        <script src="js/jquery-price-slider.js"></script>
         <!-- meanmenu JS
         ============================================ -->
-        <script src="../js/jquery.meanmenu.js"></script>
+        <script src="js/jquery.meanmenu.js"></script>
         <!-- owl.carousel JS
         ============================================ -->
-        <script src="../js/owl.carousel.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
         <!-- sticky JS
         ============================================ -->
-        <script src="../js/jquery.sticky.js"></script>
+        <script src="js/jquery.sticky.js"></script>
         <!-- scrollUp JS
         ============================================ -->
-        <script src="../js/jquery.scrollUp.min.js"></script>
+        <script src="js/jquery.scrollUp.min.js"></script>
         <!-- mCustomScrollbar JS
         ============================================ -->
-        <script src="../js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
-        <script src="../js/scrollbar/mCustomScrollbar-active.js"></script>
+        <script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+        <script src="js/scrollbar/mCustomScrollbar-active.js"></script>
         <!-- metisMenu JS
         ============================================ -->
-        <script src="../js/metisMenu/metisMenu.min.js"></script>
-        <script src="../js/metisMenu/metisMenu-active.js"></script>
+        <script src="js/metisMenu/metisMenu.min.js"></script>
+        <script src="js/metisMenu/metisMenu-active.js"></script>
         <!-- sparkline JS
         ============================================ -->
-        <script src="../js/sparkline/jquery.sparkline.min.js"></script>
-        <script src="../js/sparkline/jquery.charts-sparkline.js"></script>
+        <script src="js/sparkline/jquery.sparkline.min.js"></script>
+        <script src="js/sparkline/jquery.charts-sparkline.js"></script>
         <!-- calendar JS
         ============================================ -->
-        <script src="../js/calendar/moment.min.js"></script>
-        <script src="../js/calendar/fullcalendar.min.js"></script>
-        <script src="../js/calendar/fullcalendar-active.js"></script>
+        <script src="js/calendar/moment.min.js"></script>
+        <script src="js/calendar/fullcalendar.min.js"></script>
+        <script src="js/calendar/fullcalendar-active.js"></script>
         <!-- float JS
         ============================================ -->
-        <script src="../js/flot/jquery.flot.js"></script>
-        <script src="../js/flot/jquery.flot.resize.js"></script>
-        <script src="../js/flot/curvedLines.js"></script>
-        <script src="../js/flot/flot-active.js"></script>
+        <script src="js/flot/jquery.flot.js"></script>
+        <script src="js/flot/jquery.flot.resize.js"></script>
+        <script src="js/flot/curvedLines.js"></script>
+        <script src="js/flot/flot-active.js"></script>
         <!-- plugins JS
         ============================================ -->
-        <script src="../js/plugins.js"></script>
+        <script src="js/plugins.js"></script>
         <!-- main JS
         ============================================ -->
-        <script src="../js/main.js"></script>
+        <script src="js/main.js"></script>
 
 
 </body>
