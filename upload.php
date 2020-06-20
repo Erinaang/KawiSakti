8bdfcb9d6a9dc41f45b19a57a5def2945feb8b5c
KikiAbdullah <kikirabdullah@gmail.com>
2020-06-21 03:16:15 +0700

    delete id pengiriman, kota di upload

--- upload.php
+++ upload.php
@@ -35,7 +35,6 @@ if (isset($_FILES['bukti_ktp'])) {
    $idTrans = $_GET['ID_TRANS']; //ambil jam pesan dari URL
    $tanggal = $_POST['tanggal']; // dari form checkout
    $alamat = $_POST['alamat']; // dari form checkout
-   $kota = $_POST['kota']; // dari form checkout
    $proyek = $_POST['proyek'];
    $file_name = $_FILES['bukti_ktp']['name'];
    $file_size = $_FILES['bukti_ktp']['size'];
@@ -57,28 +56,19 @@ if (isset($_FILES['bukti_ktp'])) {
       move_uploaded_file($file_tmp, "img/Uploads/ktp/" . $file_name); //masukin ke folder
 
       //SELECT KERANJANG buat dimasukin ke tabel transaksi
-      $selectKeranjang = mysqli_query($mysqli, "SELECT SUM(JUMLAH_SET) AS jml, pk.MASA_SEWA FROM `transaksi` AS tr JOIN `transaksi_item` AS ti ON tr.ID_TRANSAKSI = ti.ID_TRANSAKSI JOIN `paket` AS pk ON ti.ID_PAKET = pk.ID_PAKET WHERE tr.ID_PENYEWA ='$idPenyewa' AND tr.ID_TRANSAKSI='$idTrans' AND tr.STATUS='checkout'") or die("data salah: " . mysqli_error($mysqli));
+      $selectKeranjang = mysqli_query($mysqli, "SELECT pk.MASA_SEWA FROM `transaksi` AS tr JOIN `transaksi_item` AS ti ON tr.ID_TRANSAKSI = ti.ID_TRANSAKSI JOIN `paket` AS pk ON ti.ID_PAKET = pk.ID_PAKET WHERE tr.ID_PENYEWA ='$idPenyewa' AND tr.ID_TRANSAKSI='$idTrans' AND tr.STATUS='checkout'") or die("data salah: " . mysqli_error($mysqli));
       while ($show = mysqli_fetch_array($selectKeranjang)) {
-         $jumlahSet = $jumlahSet + $show['jml'];
          $masaSewa = $show['MASA_SEWA'];
       }
 
       //menghitung tanggal kembali berdasarkan tanggal sewa + masa sewa
       $tgl_kembali = date('Y-m-d', strtotime('+' . $masaSewa . ' days', strtotime(str_replace('/', '-', $tanggal)))) . PHP_EOL;
 
-
-      if ($jumlahSet < 150) { //kalo jumlahnya kurang dari 150 
-         $idPengiriman = 1; //pickup
-      } elseif ($jumlahSet < 500 || $jumlahSet > 500) { //kalo kurang dari 500 dan lebih dari 500
-         $idPengiriman = 2; // truk kecil
-      } elseif ($jumlahSet > 700) { // kalo lebih dari 700
-         $idPengiriman = 3; //truk besar
-      }
       date_default_timezone_set('Asia/Jakarta'); //MENGUBAH TIMEZONE
       $time = date("Y-m-d H:i:s");
 
       //masukin data ke transaksi
-      $queryInsert = mysqli_query($mysqli, "UPDATE transaksi SET ID_PENGIRIMAN='$idPengiriman', STATUS='terkirim', BUKTI_PEMBAYARAN='$file_name_bukti', 
+      $queryInsert = mysqli_query($mysqli, "UPDATE transaksi SET STATUS='terkirim', BUKTI_PEMBAYARAN='$file_name_bukti', 
       BUKTI_KTP='$file_name', ALAMAT='$alamat', PROYEK='$proyek', JAM_PEMESANAN='$time', TGL_SEWA='$tanggal', 
       TGL_KEMBALI='$tgl_kembali' WHERE ID_TRANSAKSI='$idTrans'") or die("data salah: " . mysqli_error($mysqli));
 
