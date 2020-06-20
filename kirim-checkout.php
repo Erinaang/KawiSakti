56c1b8cff87ce02f8ea1e1a6aeb97df285d5b54b
KikiAbdullah <kikirabdullah@gmail.com>
2020-06-21 03:14:43 +0700

    id pengiriman di checkout

--- kirim-checkout.php
+++ kirim-checkout.php
@@ -8,13 +8,26 @@ if (!isset($_SESSION["username"])) { // Kalo ga login, harus ke login dulu
 include "koneksi/koneksi.php"; // ambil koneksi;
 
 $idPenyewa = $_GET['ID_PENYEWA']; //ambil id_user dari URL
-echo $jaminan = $_GET['JAMINAN'];
-
-
+$jaminan = $_GET['JAMINAN'];
+$idTrans = $_GET['ID_TRANS'];
 
 date_default_timezone_set('Asia/Jakarta'); //MENGUBAH TIMEZONE
 $time = date("Y-m-d H:i:s");
 
-$updateCheckout = mysqli_query($mysqli, "UPDATE transaksi SET STATUS = 'checkout', JAMINAN='$jaminan', JAM_PEMESANAN='$time' WHERE ID_PENYEWA='$idPenyewa'") or die("data salah: " . mysqli_error($mysqli));
+//SELECT KERANJANG buat dimasukin ke tabel transaksi
+$selectKeranjang = mysqli_query($mysqli, "SELECT SUM(JUMLAH_SET) AS jml FROM `transaksi` AS tr JOIN `transaksi_item` AS ti ON tr.ID_TRANSAKSI = ti.ID_TRANSAKSI JOIN `paket` AS pk ON ti.ID_PAKET = pk.ID_PAKET WHERE tr.ID_PENYEWA ='$idPenyewa' AND tr.ID_TRANSAKSI='$idTrans' AND tr.STATUS='checkout'") or die("data salah: " . mysqli_error($mysqli));
+while ($show = mysqli_fetch_array($selectKeranjang)) {
+   $jumlahSet = $jumlahSet + $show['jml'];
+}
+
+if ($jumlahSet < 150) { //kalo jumlahnya kurang dari 150 
+   $idPengiriman = 1; //pickup
+} elseif ($jumlahSet < 500 || $jumlahSet > 500) { //kalo kurang dari 500 dan lebih dari 500
+   $idPengiriman = 2; // truk kecil
+} elseif ($jumlahSet > 700) { // kalo lebih dari 700
+   $idPengiriman = 3; //truk besar
+}
+
+$updateCheckout = mysqli_query($mysqli, "UPDATE transaksi SET ID_PENGIRIMAN='$idPengiriman', STATUS = 'checkout', JAMINAN='$jaminan', JAM_PEMESANAN='$time' WHERE ID_PENYEWA='$idPenyewa' AND ID_TRANSAKSI='$idTrans'") or die("data salah: " . mysqli_error($mysqli));
 
 header("Location: profilBar.php"); //go to page profilbar
