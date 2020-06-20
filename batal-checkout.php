c663cca81e0a8d594a8e53d59a9f3f61984f79aa
KikiAbdullah <kikirabdullah@gmail.com>
2020-06-20 23:29:39 +0700

    batal-checkout fix

--- batal-checkout.php
+++ batal-checkout.php
@@ -1,11 +1,12 @@
 <?php 
 session_start();
 include "koneksi/koneksi.php"; // ambil koneksi
-$idPenyewa = $_GET['id_penyewa']; //ambil id_keranjang dari URL
-$jam = $_GET['jam'];
-$status= $_GET['status'];
-$queryDeleteKeranjang = mysqli_query($mysqli, "DELETE FROM keranjang WHERE id_penyewa = '$idPenyewa' AND jam_pemesanan='$jam' AND status='$status'") or die("data salah: " . mysqli_error($mysqli)); //delete keranjang berdasarkan id
+$idTrans = $_GET['ID_TRANS'];
 
-if ($queryDeleteKeranjang) {
+$deleteItem = mysqli_query($mysqli, "DELETE FROM transaksi_item WHERE ID_TRANSAKSI='$idTrans'") or die("data salah: " . mysqli_error($mysqli)); //delete keranjang berdasarkan id
+
+$deleteTransaksi = mysqli_query($mysqli, "DELETE FROM transaksi WHERE ID_TRANSAKSI='$idTrans'") or die("data salah: " . mysqli_error($mysqli)); //delete keranjang berdasarkan id
+
+if ($deleteTransaksi) {
 	header("Location: ProfilBar.php"); //go to page profilbar //go to page profilbar
 }
