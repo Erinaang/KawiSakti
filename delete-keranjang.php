e3e063978cf9ee29f4e0e9dbe521be5179cacc00
KikiAbdullah <kikirabdullah@gmail.com>
2020-06-20 21:58:21 +0700

    delete-keranjang fix

--- delete-keranjang.php
+++ delete-keranjang.php
@@ -1,9 +1,14 @@
 <?php 
 session_start();
 include "koneksi/koneksi.php"; // ambil koneksi
-$idKeranjang = $_GET['id_keranjang']; //ambil id_keranjang dari URL
-$queryDeleteKeranjang = mysqli_query($mysqli, "DELETE FROM keranjang WHERE id_keranjang = '$idKeranjang'") or die("data salah: " . mysqli_error($mysqli)); //delete keranjang berdasarkan id
+$idItem = $_GET['ID_ITEM']; //ambil id_Item dari URL
+$idTrans = $_GET['ID_TRANS']; //ambil id_Trans dari URL
+$total = $_GET['TOTAL']; //ambil total dari URL
 
-if ($queryDeleteKeranjang) {
+$kurangiTotal = mysqli_query($mysqli, "UPDATE transaksi SET TOTAL=TOTAL-'$total' WHERE ID_TRANSAKSI='$idTrans'") or die("data salah: " . mysqli_error($mysqli)); //mengurangi total di transaki
+
+$queryDeleteItem = mysqli_query($mysqli, "DELETE FROM transaksi_item WHERE ID_TRANSAKSI_ITEM = '$idItem'") or die("data salah: " . mysqli_error($mysqli)); //delete keranjang berdasarkan id
+
+if ($queryDeleteItem) {
 	header("Location: ProfilBar.php"); //go to page profilbar //go to page profilbar
 }
