b07c5dd26d6d633dcf8c9afc5aec3b03ec67bc15
KikiAbdullah <kikirabdullah@gmail.com>
2020-06-21 04:35:25 +0700

    update transaksi is null

--- admin/data-pengiriman/hapus-pengiriman.php
+++ admin/data-pengiriman/hapus-pengiriman.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION["username"])) {
 include "../connection/Connection.php";
 $idPengiriman = $_GET['ID_PENGIRIMAN']; 
 
-$queryUpdateTransaksi= mysqli_query($mysqli, "UPDATE `transaksi` SET ID_PENGIRIMAN=1 WHERE ID_PENGIRIMAN='$idPengiriman'") or die("data salah: " . mysqli_error($mysqli));
+$queryUpdateTransaksi= mysqli_query($mysqli, "UPDATE `transaksi` SET ID_PENGIRIMAN=NULL WHERE ID_PENGIRIMAN='$idPengiriman'") or die("data salah: " . mysqli_error($mysqli));
 
 $queryDeletePaket = mysqli_query($mysqli, "DELETE FROM pengiriman WHERE ID_PENGIRIMAN = '$idPengiriman'") or die("data salah: " . mysqli_error($mysqli));
 
